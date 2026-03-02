f81237e1391555a13498b2f92fac3e28ded9028d
Aleksandar Martin <aleksandar.bojic12@gmail.com>
2026-03-03 00:08:53 +0100

    feat: Configure Eclipse for Java 17, add authentication repository, and include a basic widget test.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,13 +9,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pulse/src/app.dart';
+import 'package:tremble/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(
-      const ProviderScope(child: PulseApp()),
+      const ProviderScope(child: TrembleApp()),
     );
 
     // Verify that our counter starts at 0.
